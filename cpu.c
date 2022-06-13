b2134209a254623f0979ae857ee51b17255809d2
Harshil-Walia <99045708+Harshil-Walia@users.noreply.github.com>
2022-06-13 17:18:46 -0400

    Update cpu.c

--- cpu.c
+++ cpu.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include "oslabs.h"
 struct PCB handle_process_arrival_pp(struct PCB ready_queue[], int *queue_cnt, struct PCB current_process, struct PCB new_process, int timestamp)
 {
@@ -41,4 +42,19 @@ struct PCB handle_process_arrival_pp(struct PCB ready_queue[], int *queue_cnt, s
             return new_process;
         }
     }
+}
+struct PCB handle_process_completion_pp(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, int timestamp){
+
+}
+struct PCB handle_process_arrival_srtp(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, struct PCB current_process, struct PCB new_process, int time_stamp){
+
+}
+struct PCB handle_process_completion_srtp(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, int timestamp){
+
+}
+struct PCB handle_process_arrival_rr(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, struct PCB current_process, struct PCB new_process, int timestamp, int time_quantum){
+
+}
+struct PCB handle_process_completion_rr(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, int timestamp, int time_quantum){
+
 }
